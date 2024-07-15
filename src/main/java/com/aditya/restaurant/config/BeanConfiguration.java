cfe8f5c93a8d1a1b2aa1d9dfe813945790d8e020
adityae <rifkyaditya966@gmail.com>
2024-07-15 08:40:27 +0700

    [ADD] setting Cross-Origin Resource Sharing(CORS)

--- src/main/java/com/aditya/restaurant/config/BeanConfiguration.java
+++ src/main/java/com/aditya/restaurant/config/BeanConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class BeanConfiguration {
@@ -17,5 +20,17 @@ public class BeanConfiguration {
     @Bean
     public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173"); // sesuaikan dengan URL frontend React.js
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
